affd68e308512d75bc18853096442ebaa2b90a06
mohamadalabsi <absimoda1@gmail.com>
2024-12-14 17:33:22 +0100

    startig new mongoDb backend project

---
+++ src/main/java/org/me/learning/springmongodb2/SpringMongoDb2Application.java
@@ -0,0 +1,13 @@
+package org.me.learning.springmongodb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoDb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoDb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/org/me/learning/springmongodb2/SpringMongoDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.me.learning.springmongodb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoDb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
